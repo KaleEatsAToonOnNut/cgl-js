fb35d1c09223ffa933edd72de70f743a4f4dfd68
K R <kaleeatsatoononnut@gmail.com>
2025-09-25 22:59:46 -0600

    Classes :3

---
+++ drawable.js
@@ -0,0 +1,24 @@
+class Drawables {
+	static line(x1, y1, x2, y2) {
+		ctx.moveTo(x1, y1);
+		ctx.lineTo(x2, y2);
+	}
+	static grid(w, h, ox, oy) {
+		for(let i = view.width; i > -w; i -= w) {
+			for(let j = view.height; j > -h; j -= h) {
+				Drawables.line((view.width - i) + ox, 0, (view.width - i) + ox, view.height);
+				Drawables.line(0, (view.height - j) + oy, view.width, (view.height - j) + oy);
+			}
+		}
+	}
+	static fillRect(x, y, w, h) {
+		ctx.fillRect(x, y, w, h);
+	}
+	static arc(x, y, r, deg1 = 0, deg2 = 360) {
+		let rad1 = deg1 * pi / 180;
+		let rad2 = deg2 * pi / 180;
+		ctx.moveTo(x + r, y);
+		ctx.arc(x, y, r, rad1, rad2, true);
+	}
+	
+};
--- main.js
+++ main.js
@@ -22,40 +22,6 @@ var offsetY = 0;
 var rectox = 0;
 var rectoy = 0;
 
-class Drawables {
-	static line(x1, y1, x2, y2) {
-		ctx.moveTo(x1, y1);
-		ctx.lineTo(x2, y2);
-	}
-	static grid(w, h, ox, oy) {
-		for(let i = view.width; i > -w; i -= w) {
-			for(let j = view.height; j > -h; j -= h) {
-				Drawables.line((view.width - i) + ox, 0, (view.width - i) + ox, view.height);
-				Drawables.line(0, (view.height - j) + oy, view.width, (view.height - j) + oy);
-			}
-		}
-	}
-	static fillRect(x, y, w, h) {
-		ctx.fillRect(x, y, w, h);
-	}
-	static arc(x, y, r, deg1 = 0, deg2 = 360) {
-		let rad1 = deg1 * pi / 180;
-		let rad2 = deg2 * pi / 180;
-		ctx.moveTo(x + r, y);
-		ctx.arc(x, y, r, rad1, rad2, true);
-	}
-	
-};
-
-class Cell {
-	constructor(x, y, width, height) {
-		this.x = x;
-		this.y = y;
-		this.width = width;
-		this.height = height;
-	}
-};
-
 var cellList = [];
 
 const extractDrawData = (cells) => {
